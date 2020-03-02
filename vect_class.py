98b6f5353edbbfb9435850978c93cac3d2aeb988
0dminnimda <52697657+0dminnimda@users.noreply.github.com>
2020-03-02 20:31:06 +0300

    .

--- vect_class.py
+++ vect_class.py
@@ -2,16 +2,22 @@ import numpy as np
 from math import ceil, floor, sin, cos, pi, tau, atan2
 
 class Vector:
-    def __init__(se, e, s=None, n=2):
+    def __init__(se, e=None, s=None, n=2):
         #n = se.e.shape[0]
-        se._n = n
+        se.__n = n
         if type(s) == type(np.array([])):
             se.s = s
         elif s == None:
             se.s = np.zeros(n, dtype=np.float)
         else:
             se.s = np.array(s[:n], dtype=np.float)
-        se.e = np.array(e[:n], dtype=np.float) + se.s
+        
+        if type(e) == type(np.array([])):
+            se.e = e
+        elif e == None:
+            se.e = np.zeros(n, dtype=np.float)
+        else:
+            se.e = np.array(e[:n], dtype=np.float) + se.s            
         se.ab = abs(se)
         
     def _st(se, a):
@@ -24,7 +30,7 @@ class Vector:
         return se.__str__()
         
     def __add__(se, oth):
-        e = se.ptsv()+oth.ptsv()#se.e+oth.e
+        e = se.ptsv()+oth.ptsv()
         return Vector(e, se.s)
         
     def __sub__(se, oth):
@@ -71,7 +77,7 @@ class Vector:
     def __eq__(se, oth):
         return abs(se) == abs(oth)
         
-    def __ne__(se, oth):
+    def ___ne__(se, oth):
         return abs(se) != abs(oth)
         
     def __gt__(se, oth):
@@ -82,6 +88,10 @@ class Vector:
         
     def __abs__(se):
         return np.linalg.norm(se.e-se.s)
+        
+    def sum(se, oth):
+        se.e = se.ptsvnc()+oth.ptsvnc()+se.s
+        #return Vector(e, se.s)
         
     def ang(se):
         return pi*12/16-atan2(*(se.e-se.s))
@@ -95,8 +105,27 @@ class Vector:
     def turn(se, alp, pt):
         pass
         
-    def link(se, oth):
-        se.link = oth
+    def move(se, s):
+        e = se.ptsv()
+        se.s = s.copy()
+        se.e = s+e
+        
+    def lock(se, oth):
+        se.e = oth.s
+        
+    def link(se, **args):
+        '''try:
+            se = args['seg']
+        except KeyError:pass'''
+        for k, v in args.items():
+            '''if k == "seg":
+                pass#se = v
+            else:'''
+            se.__dict__[k] = v
+        
+    def col(se):
+        #print(se.__dict__)
+        return se.lin
         
     def set_abs(se, rad):
         #rad = se.ab
@@ -108,36 +137,40 @@ class Vector:
         se.e = ar + se.s
         
     def perp(se, ar, ret=False):
-        if ret is True:
+        if bool(ret) is True:
             oth = se.copy()
-        elif ret is False:
+        elif bool(ret) is False:
             oth = se
         for i in range(len(ar)):
-            if 0 <= i <= oth._n:
+            if 0 <= i <= oth.__n:
                 oth.e = oth.s+(oth.s-oth.ptsv())[::-1]
                 if ar[i] == 0:
                     oth.e[0] = oth.s[0]-oth.ptsv()[0]
                 elif ar[i] == 1:
                     oth.e[1] = oth.s[1]-oth.ptsv()[1]
                 oth.e[i] = oth.s[i]-oth.ptsv()[i]
-        if ret is True:
+        if bool(ret) is True:
             return oth
             
     def copy(se):
-        return Vector(se.ptsv(), se.s)
+        return Vector(se.e.copy(), se.s.copy())
             
     def inv(se, *ar, ret=False):
+        ret = bool(ret)
         if ret is True:
             oth = se.copy()
             for i in ar:
-                if 0 <= i <= oth._n:
+                if 0 <= i <= oth.__n:
                     oth.e[i] = oth.s[i]-oth.ptsv()[i]
             return oth
         if ret is False:
             for i in ar:
-                if 0 <= i <= se._n:
+                if 0 <= i <= se.__n:
                     se.e[i] = se.s[i]-se.ptsv()[i]
-                    
+        
+    def draw_arrow(se, pd):
+        pd.poly(se.arrow_pts())
+                                    
     def draw(se, pd, r=1):
         pd.line(se.s, se.e, "red", r)
         #return (0, 0), (se.x, se.y)
@@ -147,6 +180,17 @@ class Vector:
         
     def ptsv(se):
         return (se.e-se.s).copy()
+        
+    def ptsvnc(se):
+        return (se.e-se.s)
+        
+    def arrow_pts(se, rat=0.15, mul=0.4):
+        vec = se.ptsv()
+        vec_s = vec*rat
+        vec_l = vec-vec_s
+        p1 = vec_l+(vec_s[1]*mul, -vec_s[0]*mul)
+        p2 = vec_l+(-vec_s[1]*mul, vec_s[0]*mul)
+        return se.s+p1, se.s+p2, se.e
         
 if __name__ == "__main__":
     v1 = Vector([3, 4], [0, 0, 0])
